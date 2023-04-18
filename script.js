30db877aadf67125f2932f04ece12f0af4bd6954
SARATHKUMAR-T <123524854+SARATHKUMAR-T@users.noreply.github.com>
2023-04-18 07:55:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+// let year = document.querySelector(".nav-col");
+// console.log(year);
+// let currentYear = new date().getFullYear();
+// console.log(currentYear);
+// year.innerHTML = currentYear;
+
+// Make Mobile Nav
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEL = document.querySelector(".header");
+console.log(btnNavEl);
+console.log(headerEL);
+
+// btnNavEl.addEventListener("click", function () {
+//   headerEL.classList.add("nav-open");
+// });
